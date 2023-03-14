9f87465f296399c81d97f0f08276854dfe2d3244
Your Name <you@example.com>
2023-03-14 15:08:45 -0300

    exemplo de requisicao server cliente

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 const PORT = 3000
 const app = express()
@@ -9,12 +10,31 @@ const app = express()
 // Update - PUT PATCH   - UPDATE
 // Delete - DELETE      - DELETE
 
-app.get("/tarefa", (req, res) => res.send("# Buscar dados de todas as tarefas"))
-app.get("/tarefa/:id", (req, res) => res.send("# Buscar dados uma tarefa"))
-app.post("/tarefa", (req, res) => res.send("# Inserir uma tarefa"))
-app.put("/tarefa", (req, res) => res.send("# Alterar todos os dados de uma tarefa"))
-app.patch("/tarefa", (req, res) => res.send("# Alterar dados específicos de uma tarefa"))
-app.delete("/tarefa/:id", (req, res) => res.send("# deletar uma tarefa"))
+app.use(cors())
+app.use(express.json())
+
+app.get("/tarefa", (req, res) => {
+  res.json([
+    {
+      title: "lavar louças",
+      date: Date.now(),
+      fineshed: false
+    }
+  ])
+})
+
+app.get("/tarefa/:id", (req, res) => res.send("# Buscar dados de uma tarefa"))
+
+app.post("/tarefa", (req, res) => {
+  console.log("recebido:", req.body)
+  res.json({ teste: "# Inserir uma tarefa"})
+})
+
+app.put("/tarefa/:id", (req, res) => res.send("# Alterar todos os dados de uma tarefa"))
+
+app.patch("/tarefa/:id", (req, res) => res.send("# Alterar dados específicos de uma tarefa"))
+
+app.delete("/tarefa/:id", (req, res) => res.send("# Excluir uma tarefa"))
 
 app.get("/", (req, res) => res.send("Hello world"))
 
